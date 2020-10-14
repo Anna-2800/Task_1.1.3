52b8b9a8ca78cb6b7689711ae9c938f597a64c76
Anna <maksimova2800@gmail.com>
2020-10-14 14:44:26 +0300

    no message

--- CoreTaskTemplate-master 2/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ CoreTaskTemplate-master 2/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -12,98 +13,29 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    Util util =  new Util();
+    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        try {
-            util.connection();
-            Statement statement = util.conn.createStatement();
-            String table = "CREATE TABLE IF NOT EXISTS base (Id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
-                    "name VARCHAR(20) NOT NULL, " +
-                    "lastname VARCHAR(20) NOT NULL, " +
-                    "age INT NOT NULL)";
-            statement.executeUpdate(table);
-            statement.close();
-        } catch (SQLException t) {
-            t.printStackTrace();
-        }
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try {
-            util.connection();
-            Statement statement = util.conn.createStatement();
-            String tableDrop = "DROP TABLE if EXISTS base";
-            statement.executeUpdate(tableDrop);
-            statement.close();
-        } catch (SQLException t) {
-            t.printStackTrace();
-        }
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            util.connection();
-            PreparedStatement statement = util.conn.prepareStatement ("INSERT INTO base (name, lastname, age) VALUES (?, ?, ?)");
-            statement.setString(1, name);
-            statement.setString(2, lastName);
-            statement.setByte(3, age);
-            statement.executeUpdate();
-            statement.close();
-            System.out.println("User с именем " + name + " добавлен в базу данных");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        try {
-            util.connection();
-            PreparedStatement statement = util.conn.prepareStatement("DELETE FROM base WHERE Id = ?");
-            statement.setLong(1, id);
-            statement.executeUpdate();
-            statement.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-
-        List<User> results = new ArrayList<>();
-
-        try {
-            util.connection();
-            String sql = "SELECT * FROM base";
-            PreparedStatement statement = util.conn.prepareStatement(sql) ;
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
-                User user = new User();
-                user.setId(resultSet.getLong(1));
-                user.setName(resultSet.getString(2));
-                user.setLastName(resultSet.getString(3));
-                user.setAge(resultSet.getByte(4));
-                System.out.println(user.toString());
-                results.add(user);
-            }
-            statement.close();
-        } catch (SQLException t) {
-            t.printStackTrace();
-        }
-        return results;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try {
-            Statement statement;
-            util.connection();
-            statement = util.conn.createStatement();
-            String tableClean = "TRUNCATE TABLE base ";
-            statement.executeUpdate(tableClean);
-            statement.close();
-        } catch (SQLException t) {
-            t.printStackTrace();
-        }
+        userDao.cleanUsersTable();
     }
 }
